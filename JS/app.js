8368019f4587c4d141c6464b7309eb594f716521
Lucia Martinez <lalumartinezzz4@gmail.com>
2024-11-10 22:51:05 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+//Suscribite a nuestro websitter!
+alert ("Suscribite a nuestro websitter!")
+const nombreUsuario = prompt ("Ingrese su nombre")
+const emailUsuario = prompt ("Ingrese su email")
+const edadUsuario = prompt ("Ingrese su edad")
+
+
+
+//Calcular total
+const total = 0;
+debugger
+function calcularTotal (precio, cantidad) {
+    debugger
+    return precio * cantidad;
+}
+debugger
+console.log (calcularTotal (10000, 3));
